2fc898343885e4c654e29c839fb32ec09cf191ce
sfratamico90 <sfratamico90@malvernprep.org>
2018-01-20 09:38:20 -0800

    Added Multiple LED chain functionality
    Added Multiple LED chain functionality

--- Arduino/sketch_jan20a/sketch_jan20a.ino
+++ Arduino/sketch_jan20a/sketch_jan20a.ino
@@ -1,29 +1,39 @@
 int led13 = 13;
+int led12 = 12;
+int led11 = 11;
 int bpm = 120;
+int top = 1300;
+int bottom = 1074;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led13, OUTPUT);
+  pinMode(led12, OUTPUT);
+  pinMode(led11, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(led13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  countChains(1);
+  // turn the LED on (HIGH is the voltage level)
   delay(60000/bpm/2);                       // wait for a second
   digitalWrite(led13, LOW);    // turn the LED off by making the voltage LOW
   delay(60000/bpm/2);      
 
 }
 
-void chain_1(){
-  
+void countChains(int i){
+  int chains = parseRange(i);
+  digitalWrite(led13, HIGH);
+  if (chains > 1){
+    digitalWrite(led12, HIGH);
+  }
+  if (chains > 2){
+    digitalWrite(led11, HIGH);
+  }
 }
 
-void chain_2(){
-  
-}
-
-void chain_3(){
-  
+int parseRange(int freq){
+  return 2;
 }
 

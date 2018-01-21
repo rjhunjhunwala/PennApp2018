eef79847e032df69206a280bbfd12c2ea53a7b27
Rohan Jhunjhunwala <rjhunjhunwala80@malvernprep.org>
2018-01-21 01:02:43 -0500

    code

--- Leap/src/leap/Leap.java
+++ Leap/src/leap/Leap.java
@@ -1,19 +1,14 @@
 package leap;
 
-
-import java.io.IOException;
 import java.lang.Math;
 import com.leapmotion.leap.*;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
@@ -21,11 +16,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Leap {
 
@@ -40,8 +31,8 @@ public class Leap {
 
 	public static final int SpeakX = 1211;
 	public static final int SpeakY = 748;
-  public static Frame lastFrame = null;
-	
+	public static Frame lastFrame = null;
+
 	public static Robot r = null;
 
 	static {
@@ -108,146 +99,159 @@ public class Leap {
 		).start();
 	}
 
-		public static int getTempo(String song){
-	try{
-	
-		
-		String query = "https://songbpm.com/";
-		String firstPart = "";
-		for(char c: song.toCharArray()){
-			if(Character.isAlphabetic(c)||Character.isDigit(c)){
-				firstPart+=Character.toLowerCase(c);
-			}else if(c==' '||c=='-'||c=='\''||c=='.'){
-				firstPart+='-';
+	public static int getTempo(String song) {
+		try {
+
+			String query = "https://songbpm.com/";
+			String firstPart = "";
+			for (char c : song.toCharArray()) {
+				if (Character.isAlphabetic(c) || Character.isDigit(c)) {
+					firstPart += Character.toLowerCase(c);
+				} else if (c == ' ' || c == '-' || c == '\'' || c == '.') {
+					firstPart += '-';
+				}
 			}
+			query += firstPart;
+			query += "?q=" + song.replaceAll(" ", "%20").replaceAll(" ", "%27");
+
+			URL url = new URL(query);
+			InputStream inputStream = null;
+			DataInputStream dataInputStream;
+
+			inputStream = url.openStream();
+
+			dataInputStream = new DataInputStream(new BufferedInputStream(inputStream));
+
+			String s;
+			while ((s = dataInputStream.readLine()) != null) {
+				//we parse HTML with regex
+				if (s.matches("[ \\t]*<p class=\"title\">[\\d]+</p>")) {
+					String out = "";
+					for (char c : s.toCharArray()) {
+						if (Character.isDigit(c)) {
+							out += c;
+						}
+
+					}
+					int ret = Integer.parseInt(out);
+					return ret;
+
+				}
+			}
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+			return 120;
+//If something goes horribly wrong, return 120. 
 		}
-		query+=firstPart;
-	 query+="?q="+song.replaceAll(" ","%20").replaceAll(" ","%27");
-	
-		URL url = new URL(query);
-		InputStream inputStream = null;
-		DataInputStream dataInputStream;
-		
-		inputStream = url.openStream();
-		
-		dataInputStream = new DataInputStream(new BufferedInputStream(inputStream));
-		
-		String s;
-		while((s=dataInputStream.readLine())!=null){
-			//we parse HTML with regex
-		if(s.matches("[ \\t]*<p class=\"title\">[\\d]+</p>")){
-		String out = "";
-			for(char c:s.toCharArray()){
-			if(Character.isDigit(c)){
-				out+=c;
-		}
-			
-		}
-			int ret = Integer.parseInt(out);
-		return ret;
-		
-		}
+		return 120;
 	}
 
-}catch(Exception ex){
-	System.out.println(ex);
-	return 120;
-//If something goes horribly wrong, return 120. 
-}
-return 120;	
-}
-		public static int sum(Color c){
-			return c.getGreen()+c.getRed()+c.getBlue();
-		}
-		public static String findSongName(){
+	public static int sum(Color c) {
+		return c.getGreen() + c.getRed() + c.getBlue();
+	}
+
+	public static String findSongName() {
 		int xEnd = xBoxEndMax;
 		String song = "Should I stay or should I go now?";
-			for(;;){
-				if(sum(r.getPixelColor(xEnd,yBoxSearch))>400){
-					break;
-				}
-				xEnd--;
+		for (;;) {
+			if (sum(r.getPixelColor(xEnd, yBoxSearch)) > 400) {
+				break;
 			}
-			xEnd -=22;
-			Rectangle rect = new Rectangle(0,yBoxSearch-15,xEnd,30);
-			int newH, newW;
-			BufferedImage b = r.createScreenCapture(rect);
-			java.awt.Image img = b.getScaledInstance(newW=b.getWidth()*3,newH=b.getHeight()*3,BufferedImage.TYPE_INT_ARGB);
-			b = new BufferedImage(newW, newH,BufferedImage.TYPE_INT_ARGB);
-			
-			Graphics2D g = b.createGraphics();
-			g.drawImage(img,0,0,null);
-			g.dispose();
-	
-			try{
-				
-			ImageIO.write(b,"png",new File("temp"));
+			xEnd--;
+		}
+		xEnd -= 22;
+		Rectangle rect = new Rectangle(0, yBoxSearch - 15, xEnd, 30);
+		int newH, newW;
+		BufferedImage b = r.createScreenCapture(rect);
+		java.awt.Image img = b.getScaledInstance(newW = b.getWidth() * 3, newH = b.getHeight() * 3, BufferedImage.TYPE_INT_ARGB);
+		b = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D g = b.createGraphics();
+		g.drawImage(img, 0, 0, null);
+		g.dispose();
+
+		try {
+
+			ImageIO.write(b, "png", new File("temp"));
 			//Runtime.getRuntime().exec(new String[]{"cmd.exe", "ocr.bat"});
 //Process p = Runtime.getRuntime().exec("tesseract temp out");
 			//while(true)System.out.println((char)p.getInputStream().read());
-  Runtime.
-   getRuntime().
-   exec("cmd /c start \"\" ocr.bat");
-	
-			}catch(Exception ex){}
-		return song;
+			Runtime.
+				getRuntime().
+				exec("cmd /c ocr.bat");
+			Thread.sleep(4500);
+			File f = new File("out.txt");
+
+			String s = new java.util.Scanner(f).nextLine();
+
+			String out = "";
+			for (char c : s.toCharArray()) {
+				if (Character.isAlphabetic(c) || c == ' ') {
+					out += c;
+				}
+			}
+
+			return out;
+		} catch (Exception ex) {
 		}
-public static final int xBoxEndMax = 280;
-public static final int yBoxSearch = 682;
+		return song;
+	}
+	public static final int xBoxEndMax = 280;
+	public static final int yBoxSearch = 682;
 
 	public static void main(String[] args) throws Exception {
 
-	Thread.sleep(5000);
-
-	System.out.println(findSongName());
 		createWaitingThread();
 		double pastX = 0;
 
 		createWaitingThread();
 		boolean wereHands = false;
 		for (;;) {
-			if(con.frame().hands().get(0).fingers().extended().count()!=2){
-			boolean nowHands = !con.frame().hands().isEmpty();
-if(lastFrame!=null){
-	if(nowHands&&!wereHands){
+			if (con.frame().hands().get(0).fingers().extended().count() != 2) {
+				boolean nowHands = !con.frame().hands().isEmpty();
+				if (lastFrame != null) {
+					if (nowHands && !wereHands) {
 
-		Thread.sleep(1000);
-		click(SpeakX, SpeakY);
-		Thread.sleep(1000);
-	}
-}
-wereHands = nowHands;
-			double newX = con.frame().hands().get(0).palmPosition().getX();
-
-			double dX = newX - pastX;
-			if (shouldSkip.get()) {
-				if (Math.abs(dX) > E && pastX != 0) {
-					shouldSkip.set(false);
-					createWaitingThread();
-					if (dX > 0) {
-						click(SKIP_FORWARD_X, SKIP_Y);
-						Thread.sleep(1000);
-						click(SpeakX, SpeakY);
-						Thread.sleep(1000);
-					} else {
-						click(SKIP_BACK_X, SKIP_Y);
-						Thread.sleep(250);
-						click(SKIP_BACK_X, SKIP_Y);
 						Thread.sleep(1000);
 						click(SpeakX, SpeakY);
 						Thread.sleep(1000);
 					}
-
 				}
-			}
-lastFrame = con.frame();
-			pastX = newX;
-			
-			if (!con.frame().hands().isEmpty()&&(mapZero > minVolHeight || con.frame().hands().get(0).fingers().extended().count() != 0)) {
-				click(BarXStart + (normalize(con.frame().hands().get(0).palmPosition().getY()) - minVolHeight) / (DH / 1.0) * (LENGTH / 1.0), BarY);
-			}
+				wereHands = nowHands;
+				double newX = con.frame().hands().get(0).palmPosition().getX();
 
+				double dX = newX - pastX;
+				if (shouldSkip.get()) {
+					if (Math.abs(dX) > E && pastX != 0) {
+						shouldSkip.set(false);
+						createWaitingThread();
+						if (dX > 0) {
+							click(SKIP_FORWARD_X, SKIP_Y);
+							Thread.sleep(1000);
+							click(SpeakX, SpeakY);
+							Thread.sleep(1000);
+						} else {
+							click(SKIP_BACK_X, SKIP_Y);
+							Thread.sleep(250);
+							click(SKIP_BACK_X, SKIP_Y);
+							Thread.sleep(1000);
+							click(SpeakX, SpeakY);
+							Thread.sleep(1000);
+						}
+String song =  findSongName();
+int tempo = getTempo(song);
+System.out.println(song+"|"+tempo+" bpm");
+					}
+				}
+				lastFrame = con.frame();
+				pastX = newX;
+
+				if (!con.frame().hands().isEmpty() && (mapZero > minVolHeight || con.frame().hands().get(0).fingers().extended().count() != 0)) {
+					click(BarXStart + (normalize(con.frame().hands().get(0).palmPosition().getY()) - minVolHeight) / (DH / 1.0) * (LENGTH / 1.0), BarY);
+				}
+
+			}
 		}
-	}
 	}
 }
